70f5304e56d64641b9830dff29e0cc3b7cd4c5d2
Eric Carvalho <ericcarvalho935@gmail.com>
2024-12-31 01:25:18 -0300

    feat: novas rotas na api (put, delete e get)

--- src/app.js
+++ src/app.js
@@ -14,6 +14,16 @@ const livros = [
     }
 ];
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id); 
+    });
+}
+
+function atualizaLivro(index, titulo) {
+    livros[index].titulo = titulo;
+}
+
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Express API');
 });
@@ -26,6 +36,35 @@ app.post("/livros", (req, res) => {
     livros.push(req.body);
 
     res.status(201).json({"message": "Livro incluído com sucesso!"});
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+
+    if (index != -1)
+        res.status(200).json(livros[index]);
+    else 
+        res.status(404).json({"message": "Livro não encontrado!"});
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+
+    if (index != -1) {
+        atualizaLivro(index, req.body.titulo);
+        res.status(200).json({"livro": livros[index], "message": "Livro atualizado com sucesso!"});
+    } else
+        res.status(404).json({"message": "Livro não encontrado!"});
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+
+    if (index != -1) {
+        livros.splice(index, 1);
+        return res.status(200).json({"message": "Livro excluído com sucesso!", "livros": livros});
+    } else 
+        res.status(404).json({"message": "Livro não encontrado!"});
 });
 
 export default app;
